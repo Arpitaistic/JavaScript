dc29fb95f26ce9b21ac6e46ae474805cd25b4dae
Arpitaistic <112889944+Arpitaistic@users.noreply.github.com>
2025-03-16 05:06:52 +0000

    datatypes

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,18 @@
+"Use Strict"; //use all js code as the newer version of JavaScript
+// alert (3*3) We are using nodeJs not browser.
+//Code readability should be high
+let age = 15;
+let yourName = "Steve"
+
+//Primitive DataType
+//number ==> 2 to power 53
+//bigint
+//string==> ""
+//boolean==> true, false
+//null ==> standalone value
+//undefined ==>
+//Symbol ==> Unique
+console.log (yourName)
+console.log (typeof(yourName))
+//console.log(typeof(undefined)) ==> Undefined
+//console.log (typeof(null)) ==> object
